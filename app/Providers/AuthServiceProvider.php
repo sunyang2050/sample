b5ac3fc3d32a78a5e84c9fac4c4bf5f526a2fc48
sunyang2050 <42016072@qq.com>
2018-12-21 08:32:52 +0000

    Add user statuses

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -20,6 +20,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
         \App\Models\User::class  => \App\Policies\UserPolicy::class,
+        \App\Models\Status::class  => \App\Policies\StatusPolicy::class,
     ];
 
     /**
